c98f8993b2340b453427ba2df04d7ea97e272f4c
Luke Rooney <lukerooney7@outlook.com>
2024-11-01 12:02:47 +0000

    Pushing messy working copies for debugging

--- pypmt/propagators/existsBasic.py
+++ pypmt/propagators/existsBasic.py
@@ -1,5 +1,7 @@
 import networkx as nx
 import z3
+from networkx import NetworkXNoCycle
+
 from pypmt.utilities import log
 
 
@@ -8,40 +10,78 @@ class ExistsBasicUserPropagator(z3.UserPropagateBase):
         z3.UserPropagateBase.__init__(self, s, ctx)
         self.add_fixed(lambda x, v: self._fixed(x, v))
         self.encoder = e
+        self.add_final(lambda : self._final())
         self.graph = self.encoder.modifier.graph
         self.current = [nx.DiGraph()]
         self.stack = []
-        self.numbers = {a: i for i, a in enumerate(self.graph)}
+        # self.numbers = {a: i for i, a in enumerate(self.graph)}
 
     def push(self):
+        log("push",1)
         new = []
         for graph in self.current:
             new.append(graph.copy())
+            if len(graph.nodes) == 2 and len(graph.edges) == 2:
+                print("H")
         self.stack.append(new)
 
     def pop(self, n):
+        log("pop", 1)
         for _ in range(n):
             self.current = self.stack.pop()
 
+    def _final(self):
+        for graph in self.current:
+            if len(list(nx.simple_cycles(graph))) > 0:
+                print("FINISHED WITH CYCLE")
+        for s in self.stack:
+            for graph in s:
+                if len(list(nx.simple_cycles(graph))) > 0:
+                    print("FINISHED WITH CYCLE")
+
     def _fixed(self, action, value):
+
         if value:
+            log("fixed start", 1)
             actions = str(action).split('_')
             step = int(actions[-1])
+
             action_name = '_'.join(actions[:-1])
             while step >= len(self.current):
                 self.current.append(nx.DiGraph())
+            cycles = list(nx.simple_cycles(self.current[step]))
+            if len(cycles) > 0:
+                print(f"{len(cycles)} cycles found early")
             literals = set()
             self.current[step].add_node(action_name)
-            edges = list(self.graph.in_edges(action_name))
-            edges[0:0] = list(self.graph.edges(action_name))
+            edges = list(self.graph.in_edges(action_name)) + list(self.graph.edges(action_name))
+
             for a1, a2 in edges:
                 if a2 in self.current[step] and a1 in self.current[step]:
                     self.current[step].add_edge(a1, a2)
-            for act_1, act_2 in self.current[step].edges():
-                if self.numbers[act_1] < self.numbers[act_2]:
-                    literals.add(self.encoder.get_action_var(act_1, step))
-                    literals.add(self.encoder.get_action_var(act_2, step))
-                    break
+            try:
+                cycle = nx.find_cycle(G=self.current[step], source=action_name)
+                if cycle:
+                    for a, b in cycle:
+
+                        # if numbers[a] > numbers[b]:
+                        literals.add(self.encoder.get_action_var(a, step))
+                        literals.add(self.encoder.get_action_var(b, step))
+                            # self.current[step].remove_node(a)
+                print("here")
+            except NetworkXNoCycle:
+                if len(list(nx.simple_cycles(self.current[step]))) > 0:
+                    print("HERE")
+                pass
             if literals:
-                # literals.add(action)
+                log("fixed invalid", 1)
                 self.conflict(deps=list(literals), eqs=[])
+                # self.current[step].remove_node(action_name)
+                cycles = list(nx.simple_cycles(self.current[step]))
+                if len(cycles) > 0:
+                    print(f"{len(cycles)} cycles found before leaving invalid")
+            else:
+                log("fixed valid", 1)
+                cycles = list(nx.simple_cycles(self.current[step]))
+                if len(cycles) > 0:
+                    print(f"{len(cycles)} cycles found before leaving valid")
--- pypmt/propagators/forallBasic.py
+++ pypmt/propagators/forallBasic.py
@@ -20,6 +20,15 @@ class ForallBasicUserPropagator(z3.UserPropagateBase):
         for _ in range(n):
             self.current = self.stack.pop()
 
+    def _final(self):
+        for graph in self.current:
+            if len(list(nx.simple_cycles(graph))) > 0:
+                print("FINISHED WITH edges")
+        for s in self.stack:
+            for graph in s:
+                if len(list(nx.simple_cycles(graph))) > 0:
+                    print("FINISHED WITH edges")
+
     def _fixed(self, action, value):
         if value:
             actions = str(action).split('_')
@@ -28,17 +37,21 @@ class ForallBasicUserPropagator(z3.UserPropagateBase):
             while step >= len(self.current):
                 self.current.append(nx.DiGraph())
             literals = set()
+            if len(self.current[step].edges) > 0:
+                print(f'{len(self.current[step].edges)} edges found at start')
             self.current[step].add_node(action_name)
             for a1, a2 in list(self.graph.edges(action_name)):
                 action_2 = self.encoder.get_action_var(a2, step)
                 if a2 in self.current[step]:
                     self.current[step].add_edge(a1, a2)
                     literals.add(action_2)
+                    # self.conflict(deps=[action_2, action], eqs=[])
             for a1, a2 in list(self.graph.in_edges(action_name)):
                 action_1 = self.encoder.get_action_var(a1, step)
                 if a1 in self.current[step]:
                     self.current[step].add_edge(a1, a2)
                     literals.add(action_1)
+                    # self.conflict(deps=list([action_1, action]), eqs=[])
             if literals:
                 literals.add(action)
                 self.conflict(deps=list(literals), eqs=[])
