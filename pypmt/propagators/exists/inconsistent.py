32dfe4adea88b95c961301a095cad547c2deef39
Luke Rooney <lukerooney7@outlook.com>
2024-11-12 12:12:50 +0000

    Added inconsistent flag to lazy exists

--- pypmt/propagators/exists/inconsistent.py
+++ pypmt/propagators/exists/inconsistent.py
@@ -2,7 +2,7 @@ import networkx as nx
 import z3
 from collections import defaultdict
 
-class ExistsOptimalPropagator(z3.UserPropagateBase):
+class ExistsInconsistentPropagator(z3.UserPropagateBase):
     def __init__(self, s, ctx=None, e=None):
         z3.UserPropagateBase.__init__(self, s, ctx)
         self.add_fixed(lambda x, v: self._fixed(x, v))
