582c3d50ec0c0336951f9f4f12de09afa3b67116
Luke Rooney <lr225@st-andrews.ac.uk>
2024-12-03 12:55:44 +0000

    Further solve stats update

--- pypmt/solveStats.py
+++ pypmt/solveStats.py
@@ -10,15 +10,18 @@ def get_problem(name):
 
 def save_stats(planner):
     relevant_attrs = {"propagations", "user-propagations", "decisions", "final-checks", "conflicts"}
-    output_file = "/Users/lukeroooney/Desktop/Dissertation/parallelSAT/stats.csv"
+    output_file = "/cs/home/lr225/Documents/Lazy-Interference-SMT-Planning/stats.csv"
     file_exists = os.path.isfile(output_file)
     lines = planner.solver.statistics().__str__().strip("()").strip().split("\n")
     stats_dict = {}
+    for field in relevant_attrs:
+        stats_dict.setdefault(field, 0)
     for line in lines:
         key, value = line.strip().split(maxsplit=1)
         key = key.strip(":")
         if key in relevant_attrs:
             stats_dict[key] = value
+    
     domain, instance = get_problem(planner.encoder.task.name)
     stats_dict['domain'] = domain
     stats_dict['length'] = len(planner.solution)
@@ -28,8 +31,7 @@ def save_stats(planner):
     stats_dict['propagator'] = planner.propagator.name
     # fieldnames = ['domain', 'length', 'steps', 'mutexes', 'instance', 'propagator']  # Predefined field names
     # for field in fieldnames:
-    for field in relevant_attrs:
-        stats_dict.setdefault(field, 0)
+    
     with open(output_file, mode="a", newline="") as file:
         writer = csv.DictWriter(file, fieldnames=stats_dict.keys())
         if not file_exists:
