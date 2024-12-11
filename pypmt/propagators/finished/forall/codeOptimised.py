337c701ef1ebdf521c4f137ac76d90dc61acd387
Luke Rooney <lukerooney7@outlook.com>
2024-12-11 13:55:44 +0000

    Added trail to optimised forall

--- pypmt/propagators/finished/forall/codeOptimised.py
+++ pypmt/propagators/finished/forall/codeOptimised.py
@@ -1,22 +1,29 @@
 import z3
 
 
-class ForallCodePropagator(z3.UserPropagateBase):
+class TestPropagator(z3.UserPropagateBase):
     def __init__(self, s, ctx=None, e=None):
         z3.UserPropagateBase.__init__(self, s, ctx)
         self.add_fixed(lambda x, v: self._fixed(x, v))
         self.encoder = e
         self.graph = self.encoder.modifier.graph
         self.current = [set()]  # Use a set instead of a NetworkX graph
-        self.stack = []
+        self.trail = []  # Trail to record changes
+        self.levels = []
         self.consistent = True
 
     def push(self):
-        self.stack.append([graph.copy() for graph in self.current])
+        self.levels.append(len(self.trail))
 
     def pop(self, n):
         for _ in range(n):
-            self.current = self.stack.pop()
+            if self.levels:
+                # Find the start of the current decision level
+                level_start = self.levels.pop()
+                # Undo all changes recorded after this level
+                while len(self.trail) > level_start:
+                    step, action = self.trail.pop()
+                    self.current[step].remove(action)
         self.consistent = True
     def _fixed(self, action, value):
         if value and self.consistent:
@@ -28,11 +35,12 @@ class ForallCodePropagator(z3.UserPropagateBase):
                 while step >= len(self.current):
                     self.current.append(set())
                 self.current[step].add(action_name)
+                self.trail.append((step, action_name))
                 # There cannot be any interference: no other actions in step are True
                 return
             literals = set()
+            self.trail.append((step, action_name))
             self.current[step].add(action_name)
-            # Checking and adding out nodes using set intersection
             for dest in self.current[step] & set(self.graph.neighbors(action_name)):
                 literals.add(self.encoder.get_action_var(dest, step))
                 self.consistent = False
